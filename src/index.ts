15deca96f9a8de662a11f01dc789e5b8a8a14f96
PeteGabriel <pedrogabriel@protonmail.ch>
2020-06-02 11:12:08 +0200

    Add graphiql playground.

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,12 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import {EventsResolver} from "./queries/EventsResolver";
+import * as path from 'path'
+import * as dotenv from 'dotenv';
 
 (async () => {
   const app = express();
+  dotenv.config()
 
   const options = await getConnectionOptions(
     process.env.NODE_ENV || "development"
@@ -23,6 +26,11 @@ import {EventsResolver} from "./queries/EventsResolver";
 
   apolloServer.applyMiddleware({ app, cors: false });
   const port = process.env.PORT || 4000;
+
+  app.get('/', (_req, res) => {
+    res.sendFile(path.join(__dirname + '/static/index.html'));
+  })
+
   app.listen(port, () => {
     console.log(`server started at http://localhost:${port}/graphql`);
   });
