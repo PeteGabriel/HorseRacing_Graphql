7091bdc5d3b0979f4c4905f40835e756d021cd93
PeteGabriel <pedrogabriel@protonmail.ch>
2020-06-05 10:54:09 +0200

    Add public resources.

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,23 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import {EventsResolver} from "./queries/EventsResolver";
-import * as path from 'path'
 import * as dotenv from 'dotenv';
 
 (async () => {
   const app = express();
   dotenv.config()
+  app.use(express.static('public'))
 
   const options = await getConnectionOptions(
     process.env.NODE_ENV || "development"
   );
-  await createConnection({ ...options, name: "default" });
+  let conn = await createConnection({ ...options, name: "default" });
+  if(!conn.isConnected){
+    console.log("Connection not created.")
+    return
+  }else{
+    console.log("Connected")
+  }
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
@@ -26,10 +32,6 @@ import * as dotenv from 'dotenv';
 
   apolloServer.applyMiddleware({ app, cors: false });
   const port = process.env.PORT || 4000;
-
-  app.get('/', (_req, res) => {
-    res.sendFile(path.join(__dirname + '/static/index.html'));
-  })
 
   app.listen(port, () => {
     console.log(`server started at http://localhost:${port}/graphql`);
